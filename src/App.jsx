9305f45e7829852c51f1a42a764d2f718396cd03
SaadAhmed-coder <khaskhelisaad20@gmail.com>
2025-03-25 00:10:57 +0500

    Second Time Change is same file

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from "react";
+import { LanguageServiceMode } from "typescript";
 function App() {
-  const [count, setCount] = useState(0);
-
+  const [text, settext] = useState(" ");
+  function handleChnage(e) {
+    settext(e.target.value);
+  }
+  function handleClick() {
+    settext(text);
+  }
   return (
     <>
       <h1>Hello World!</h1>
+      <input
+        type="text"
+        placeholder="Enter your name"
+        onChange={handleChnage}
+      ></input>
+      <button onClick={handleClick}>Submit</button>
+      <label>{settext}</label>
     </>
   );
 }
